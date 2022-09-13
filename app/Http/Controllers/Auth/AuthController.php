696301a1df69aa056a4331eab4d44fd54c7342d6
Velfin Velasquez <velfinvelasquez@gmail.com>
2022-09-13 13:17:53 -0500

    metodo login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
@@ -22,11 +24,32 @@ class AuthController extends Controller
         $user->name = $request->name;
         $user->email = $request->email;
         $user->password = Hash::make($request->password);
-
         $user->save();
+
+        $token = JWTAuth::fromUser($user);
 
         return response()->json([
             "user" => $user,
+            "token" => $token
         ], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credential = $request->only('email', 'password');
+        try {
+
+            if (!$token = JWTAuth::attempt($credential)) {
+                return response()->json([
+                    "error" => "invalid credentials"
+                ], 400);
+            }
+        } catch (JWTException $ex) {
+            return response()->json([
+                "error" => "password or email invalaid"
+            ], 500);
+        }
+
+        return response()->json(compact('token'));
     }
 }
